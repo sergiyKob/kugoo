fa0d7dd84f7107be2234af1f29fde71284a496b4
sergiyKob <sergiykob799@gmail.com>
2023-09-21 15:56:17 +0300

    add popular and testimonial card

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,7 @@ const swiper = new Swiper('.swiper', {
 });
 
 
-// $(function () {
+$(function () {
 
 //    // --- scroll-up ---//
 
@@ -112,24 +112,24 @@ const swiper = new Swiper('.swiper', {
 
 //    //  slider revews  //
 
-//    $('.reviews__slider').slick({
-//       prevArrow: '<button type="button" class="slick-prev"><svg class="slick-arrow slick-arrow-prev "><use xlink: href = "images/sprite.svg#arrow-left"></use></svg></button>',
+   // $('.reviews__slider').slick({
+   //    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-arrow slick-arrow-prev "><use xlink: href = "images/sprite.svg#arrow-left"></use></svg></button>',
 
-//       nextArrow: '<button type="button" class="slick-next"><svg class="slick-arrow slick-arrow-next"><use xlink: href = "images/sprite.svg#arrow-left"></use></svg ></button>',
-//       dots: true,
-//       slidesToShow: 2,
-//       slidesToScroll: 1,
-//       responsive: [
-//          {
-//             breakpoint: 992,
-//             settings: {
-//                slidesToShow: 1,
-//                slidesToScroll: 1,
-//                arrows: false
-//             }
-//          }
-//       ]
-//    });
+   //    nextArrow: '<button type="button" class="slick-next"><svg class="slick-arrow slick-arrow-next"><use xlink: href = "images/sprite.svg#arrow-left"></use></svg ></button>',
+   //    dots: true,
+   //    slidesToShow: 2,
+   //    slidesToScroll: 1,
+   //    responsive: [
+   //       {
+   //          breakpoint: 992,
+   //          settings: {
+   //             slidesToShow: 1,
+   //             slidesToScroll: 1,
+   //             arrows: false
+   //          }
+   //       }
+   //    ]
+   // });
 
 
 
@@ -171,8 +171,12 @@ const swiper = new Swiper('.swiper', {
 
 //    //=======================//
 
+//----- testimonial rating -----//
+   $(".testimonial-card__rating").rateYo({
+      starWidth: "14px",
+      spacing: "5px",
+      normalFill: "rgba(255, 255, 255, 0.50)"
+   });
 
 
-
-
-// });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ function scripts() {
       'node_modules/swiper/swiper-bundle.min.js',
 
       // 'node_modules/slick-carousel/slick/slick.js',
-      // 'node_modules/rateyo/src/jquery.rateyo.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
       //  'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
       //  'node_modules/mixitup/dist/mixitup.min.js',
       //   'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
